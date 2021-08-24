b1a2b7487be2c7fe278e0d61ed03f13854e2a9eb
Insomnia-warpaint <wp_190694108@163.com>
2021-08-24 20:35:38 +0800

    update

---
+++ sweetie.c
@@ -0,0 +1,16 @@
+//
+// Created by insomnia on 2021/8/24.
+//
+#include "bool.h"
+#include "stdio.h"
+#define NUMBER 5
+int main(void){
+    int count;
+
+    for(count = 1 ; count <= NUMBER ; ++count){
+        printf("By my valentine!\n");
+    }
+
+    return SUCCESS;
+}
+
