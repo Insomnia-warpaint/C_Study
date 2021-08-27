f74a7966384d531964d64f475c3ea9314187534d
Insomnia-warpaint <wp_190694108@163.com>
2021-08-27 22:49:36 +0800

    update

--- sweetie.c
+++ sweetie.c
@@ -6,9 +6,11 @@
 #define NUMBER 5
 int main(void){
     int count;
+    const int limit = NUMBER;
 
-    for(count = 1 ; count <= NUMBER ; ++count){
+    for(count = 1 ; count <= limit; ++count){
         printf("By my valentine!\n");
+        printf(EOF);
     }
 
     return SUCCESS;
